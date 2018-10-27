26d4f43d6a41c39c944f6c279e00c70a4d3f3d95
Ionut Enache <enachestoianionut@gmail.com>
2018-10-27 10:01:33 +0300

    declare all the constants ids for commands and package in a single class

---
+++ FileEncodingConvertor/FileEncodingConvertor/ExtensionConstants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FileEncodingConvertor
+{
+  public class ExtensionConstants
+  {
+    #region Members
+
+    /// <summary>
+    /// Command ID.
+    /// </summary>
+    public const int UTF8_ID = 0x0100;
+
+
+    /// <summary>
+    /// Command menu group (command set GUID).
+    /// </summary>
+    public static readonly Guid CommandSet = new Guid("649bac70-70e9-40dd-9ffc-cb1877601e22");
+
+
+    #endregion
+  }
+}
