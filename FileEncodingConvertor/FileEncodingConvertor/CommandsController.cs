00d8c89a687b1eaa060f2080a6fe37bcb26aab29
Ionut Enache <enachestoianionut@gmail.com>
2018-10-27 10:03:27 +0300

    implement a commands controller class

---
+++ FileEncodingConvertor/FileEncodingConvertor/CommandsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FileEncodingConvertor
+{
+  public class CommandsController
+  {
+    #region Members
+
+    private ConvertToUTF8 mConvertToUTF8Cmd = new ConvertToUTF8();
+
+    #endregion
+
+
+
+    #region Public Methods
+
+
+    public void ChooseCommand(object sender, EventArgs args)
+    {
+      var menuCommand = sender as System.ComponentModel.Design.MenuCommand;
+      if (menuCommand == null)
+        return;
+
+      switch (menuCommand.CommandID.ID)
+      {
+        case ExtensionConstants.UTF8_ID:
+          mConvertToUTF8Cmd.Execute();
+          break;
+
+      }
+    }
+
+
+    #endregion
+  }
+}
