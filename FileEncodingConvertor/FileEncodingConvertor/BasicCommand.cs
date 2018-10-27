c38ef228d5969150910834417518177fe6c2c4da
Ionut Enache <enachestoianionut@gmail.com>
2018-10-27 10:02:56 +0300

    refactor the UTF-8 command

--- FileEncodingConvertor/FileEncodingConvertor/BasicCommand.cs
+++ FileEncodingConvertor/FileEncodingConvertor/BasicCommand.cs
@@ -3,8 +3,6 @@ using EnvDTE80;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FileEncodingConvertor
 {
--- FileEncodingConvertor/FileEncodingConvertor/ConvertToUTF8.cs
+++ FileEncodingConvertor/FileEncodingConvertor/ConvertToUTF8.cs
@@ -1,109 +1,30 @@
-﻿using System;
-using System.ComponentModel.Design;
-using System.Globalization;
-using System.IO;
+﻿using System.IO;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using EnvDTE;
-using EnvDTE80;
-using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
-using Task = System.Threading.Tasks.Task;
 
 namespace FileEncodingConvertor
 {
   /// <summary>
   /// Command handler
   /// </summary>
-  internal sealed class ConvertToUTF8 : BasicCommand
+  public sealed class ConvertToUTF8 : BasicCommand
   {
-    /// <summary>
-    /// Command ID.
-    /// </summary>
-    public const int CommandId = 0x0100;
+    #region Public Methods
+
 
     /// <summary>
-    /// Command menu group (command set GUID).
+    /// Rewrite all the file in the UTF-8 encoding
     /// </summary>
-    public static readonly Guid CommandSet = new Guid("649bac70-70e9-40dd-9ffc-cb1877601e22");
-
-    /// <summary>
-    /// VS Package that provides this command, not null.
-    /// </summary>
-    private readonly AsyncPackage package;
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="ConvertToUTF8"/> class.
-    /// Adds our command handlers for menu (commands must exist in the command table file)
-    /// </summary>
-    /// <param name="package">Owner package, not null.</param>
-    /// <param name="commandService">Command service to add command to, not null.</param>
-    private ConvertToUTF8(AsyncPackage package, OleMenuCommandService commandService)
-    {
-      this.package = package ?? throw new ArgumentNullException(nameof(package));
-      commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
-
-      var menuCommandID = new CommandID(CommandSet, CommandId);
-      var menuItem = new MenuCommand(this.Execute, menuCommandID);
-      commandService.AddCommand(menuItem);
-    }
-
-    /// <summary>
-    /// Gets the instance of the command.
-    /// </summary>
-    public static ConvertToUTF8 Instance
-    {
-      get;
-      private set;
-    }
-
-    /// <summary>
-    /// Gets the service provider from the owner package.
-    /// </summary>
-    private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
-    {
-      get
-      {
-        return this.package;
-      }
-    }
-
-    /// <summary>
-    /// Initializes the singleton instance of the command.
-    /// </summary>
-    /// <param name="package">Owner package, not null.</param>
-    public static async Task InitializeAsync(AsyncPackage package)
-    {
-      // Switch to the main thread - the call to AddCommand in ConvertToUTF8's constructor requires
-      // the UI thread.
-      await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
-
-      OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
-      Instance = new ConvertToUTF8(package, commandService);
-    }
-
-    /// <summary>
-    /// This function is the callback used to execute the command when the menu item is clicked.
-    /// See the constructor to see how the menu item is associated with this function using
-    /// OleMenuCommandService service and MenuCommand class.
-    /// </summary>
-    /// <param name="sender">Event sender.</param>
-    /// <param name="e">Event args.</param>
-    private void Execute(object sender, EventArgs e)
+    public void Execute()
     {
       var selectedItems = GetSelectedFiles();
 
-      if (!VsServiceProvider.TryGetService(typeof(DTE), out object dte))
-        return;
-
-      var dte2 = dte as DTE2;
-      foreach(var item in selectedItems)
+      foreach (var item in selectedItems)
       {
         var fileContent = File.ReadAllText(item);
         File.WriteAllText(item, fileContent, Encoding.UTF8);
       }
+
+      #endregion
 
     }
   }
